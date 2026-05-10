5cd5c7448860299e8625616c68c63ace9d00b1b1
LuckyToaster <senchymarques@hotmail.com>
2026-05-10 18:00:07 +0200

    feat: added directory tree parsing logic

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
-from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv
-from utils import spinner_task
+from os.path import expanduser
+from utils import spinner_task, get_filepaths, get_documents, generate_embeddings
 
-
-def get
-
-def get_pdfs(path: str):
-    pdf_paths = list(Path(path).iterdir())
-    docs = []
-    for p in pdf_paths:
-        docs.extend(PyPDFLoader(str(p)).load())
-    return docs
-
-def generate_embeddings(model, docs, db_dir, db_table, chunk_size, chunk_overlap): 
-    chunker = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, add_start_index=True)
-    chunks = chunker.split_documents(docs)
-    vector_store = Chroma(collection_name=db_table, persist_directory=db_dir, embedding_function=model)
-
-    chroma_max_chunks = 5461 # the maximum number of chunks you can pass to chroma.add_documents()
-    for i in range(0, len(chunks), chroma_max_chunks):
-        batch = chunks[i:i+chroma_max_chunks]
-        vector_store.add_documents(batch)
-
+DOCUMENT_FILE_SUFFIXES = ['.txt', '.md', '.docx', '.pdf', '.ipynb']
+DATA_FILE_SUFFIXES = ['.toml', '.json', '.yml' ]
+CODE_FILE_SUFFIXES = ['.py', '.c', '.h', '.cpp', '.hpp', '.html', '.js', '.css']
+SUFFIXES = DOCUMENT_FILE_SUFFIXES + DATA_FILE_SUFFIXES + CODE_FILE_SUFFIXES
 
 if __name__ == '__main__':
     load_dotenv()
-    EMBEDDING_MODEL = getenv('EMBEDDING_MODEL')
+    EMBEDDING_MODEL = getenv('EMBEDDING_MODEL', '')
     CHUNK_SIZE = int(getenv('CHUNK_SIZE', 0))
     CHUNK_OVERLAP = int(getenv('CHUNK_OVERLAP', 0))
     EMBEDDING_OUTPUT_DIMS = int(getenv('EMBEDDING_OUTPUT_DIMS', 0))
@@ -38,17 +19,18 @@ if __name__ == '__main__':
     EMBEDDING_DB_TABLE_NAME = getenv('EMBEDDING_DB_TABLE_NAME', '')
     
     # get docs -> this time all text files that are code files
+    # suffixes = ['.pdf', '.py']
+    dirs_to_ignore = ['env', 'venv']
+
+    with spinner_task('Gathering all files'):
+        starting_dir = expanduser('~')
+        file_paths = get_filepaths(starting_dir, SUFFIXES, dirs_to_ignore)
 
     with spinner_task('Initializing embedding model'):
         model = GoogleGenerativeAIEmbeddings(model=EMBEDDING_MODEL, output_dimensionality=EMBEDDING_OUTPUT_DIMS)
 
+    # with spinner_task('Generating documents objects from files'):
+    #     docs = get_documents(file_paths)
 
-    with spinner_task('Generating embeddings'):
-        generate_embeddings(
-            model = model,
-            docs = 'IDK LOL',
-            db_dir = EMBEDDING_DB_DIR,
-            db_table = EMBEDDING_DB_TABLE_NAME,
-            chunk_size = CHUNK_SIZE,
-            chunk_overlap = CHUNK_OVERLAP
-        )
+    # with spinner_task('Generating embeddings'):
+    #     generate_embeddings(model, docs, EMBEDDING_DB_DIR, EMBEDDING_DB_TABLE_NAME, CHUNK_SIZE, CHUNK_OVERLAP)
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_docling.loader import DoclingLoader
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+
 from contextlib import contextmanager
 from halo import Halo
+
+from pathlib import Path
+from os import walk
+from os.path import join
 
 @contextmanager
 def spinner_task(text, spinner='dots'):
@@ -7,3 +17,38 @@ def spinner_task(text, spinner='dots'):
     try: yield spinner
     finally: spinner.succeed(text)
 
+
+# https://search.brave.com/search?q=os.walk+python
+def get_filepaths(starting_dir: str, suffixes: list[str], dirs_to_ignore: list[str] | None) -> list[str]:
+    if not Path(starting_dir).is_dir():
+        raise Exception('starting dir is not a directory (does not exist)')
+
+    file_paths = []
+    for root, dirs, files in walk(starting_dir): 
+        dirs[:] = [d for d in dirs if not d.startswith('.')] 
+
+        if dirs_to_ignore:
+            dirs[:] = [dir for dir in dirs if not any(dir == d for d in dirs_to_ignore)] 
+
+        for f in files:
+            if any(f.endswith(s) for s in suffixes):
+                file_paths.append(join(root, f))
+    return file_paths
+
+
+def get_documents(paths: list[str]) -> list[Document]:
+    docs = []
+    for p in paths:
+        if p.endswith('.pdf'): docs.extend(PyPDFLoader(p).load())
+        else: docs.extend(DoclingLoader(p).load())
+    return docs
+
+
+def generate_embeddings(model, docs, db_dir, db_table, chunk_size, chunk_overlap): 
+    chunker = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, add_start_index=True)
+    chunks = chunker.split_documents(docs)
+    vector_store = Chroma(collection_name=db_table, persist_directory=db_dir, embedding_function=model)
+    chroma_max_chunks = 5461 # the maximum number of chunks you can pass to chroma.add_documents()
+    for i in range(0, len(chunks), chroma_max_chunks):
+        batch = chunks[i:i+chroma_max_chunks]
+        vector_store.add_documents(batch)
